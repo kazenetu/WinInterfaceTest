ab0b0b2545366d961efdf652a991b6545330d35a
kazenetu <rakudafactory@gmail.com>
2017-11-11 16:56:05 +0900

    プロパティの型を設定

--- WinInterfaceTest/SampleIntarfaceList/ClassBEX.vb
+++ WinInterfaceTest/SampleIntarfaceList/ClassBEX.vb
@@ -7,11 +7,11 @@
         Me.target = target
     End Sub
 
-    Public Property Name Implements ICommonClass.Name
+    Public Property Name As String Implements ICommonClass.Name
         Get
             Return target.Name
         End Get
-        Set(value)
+        Set(value As String)
             target.Name = value
         End Set
     End Property
