36d2c40e1ab36374bae359a1a6724d2cc220a5a6
kazenetu <rakudafactory@gmail.com>
2017-11-12 18:58:57 +0900

    メソッドコメント追加

--- WinInterfaceTest/Utility/ClassUtility.vb
+++ WinInterfaceTest/Utility/ClassUtility.vb
@@ -1,16 +1,41 @@
 ﻿Public Class ClassUtility
 
+    ''' <summary>
+    ''' ClassA作成メソッド
+    ''' </summary>
+    ''' <param name="target">対象インスタンス</param>
+    ''' <returns>ICommonClassインスタンス</returns>
+    ''' <remarks>専用EXクラスを生成</remarks>
     Public Shared Function Create(ByRef target As ClassA) As ICommonClass
         Return New ClassAEX(target)
     End Function
 
+    ''' <summary>
+    ''' ClassB作成メソッド
+    ''' </summary>
+    ''' <param name="target">対象インスタンス</param>
+    ''' <returns>ICommonClassインスタンス</returns>
+    ''' <remarks>専用EXクラスを生成</remarks>
     Public Shared Function Create(ByRef target As ClassB) As ICommonClass
         Return New ClassBEX(target)
     End Function
 
+    ''' <summary>
+    ''' ClassA作成メソッド
+    ''' </summary>
+    ''' <param name="target">対象インスタンス</param>
+    ''' <returns>ICommonClassインスタンス</returns>
+    ''' <remarks>共通EXクラスを生成</remarks>
     Public Shared Function CreateEx(ByRef target As ClassA) As ICommonClass
         Return New ClassEx(Of ClassA)(target)
     End Function
+
+    ''' <summary>
+    ''' ClassB作成メソッド
+    ''' </summary>
+    ''' <param name="target">対象インスタンス</param>
+    ''' <returns>ICommonClassインスタンス</returns>
+    ''' <remarks>共通EXクラスを生成</remarks>
     Public Shared Function CreateEx(ByRef target As ClassB) As ICommonClass
         Return New ClassEx(Of ClassB)(target)
     End Function
