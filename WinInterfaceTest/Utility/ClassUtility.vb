227f5da5d4197a562ec56ac7acdac34f3c1e8017
kazenetu <rakudafactory@gmail.com>
2017-11-12 18:58:12 +0900

    インターフェイスインスタンス作成ユーティリティ追加

---
+++ WinInterfaceTest/Utility/ClassUtility.vb
@@ -0,0 +1,18 @@
+﻿Public Class ClassUtility
+
+    Public Shared Function Create(ByRef target As ClassA) As ICommonClass
+        Return New ClassAEX(target)
+    End Function
+
+    Public Shared Function Create(ByRef target As ClassB) As ICommonClass
+        Return New ClassBEX(target)
+    End Function
+
+    Public Shared Function CreateEx(ByRef target As ClassA) As ICommonClass
+        Return New ClassEx(Of ClassA)(target)
+    End Function
+    Public Shared Function CreateEx(ByRef target As ClassB) As ICommonClass
+        Return New ClassEx(Of ClassB)(target)
+    End Function
+
+End Class
