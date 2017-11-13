35e1e855f2a12d3b21db8aa6a2fb8008ae40d436
kazenetu <rakudafactory@gmail.com>
2017-11-13 22:26:22 +0900

    ClassEx作成ファクトリクラスを追加

---
+++ WinInterfaceTest/SampleGeneric/ClassEXFactory.vb
@@ -0,0 +1,26 @@
+﻿''' <summary>
+''' ClassEx作成ファクトリクラス
+''' </summary>
+Public Class ClassEXFactory
+
+    ''' <summary>
+    ''' ClassA作成メソッド
+    ''' </summary>
+    ''' <param name="target">対象インスタンス</param>
+    ''' <returns>ICommonClassインスタンス</returns>
+    ''' <remarks>共通EXクラスを生成</remarks>
+    Public Shared Function Create(ByRef target As ClassA) As ICommonClass
+        Return New ClassEx(Of ClassA)(target)
+    End Function
+
+    ''' <summary>
+    ''' ClassB作成メソッド
+    ''' </summary>
+    ''' <param name="target">対象インスタンス</param>
+    ''' <returns>ICommonClassインスタンス</returns>
+    ''' <remarks>共通EXクラスを生成</remarks>
+    Public Shared Function Create(ByRef target As ClassB) As ICommonClass
+        Return New ClassEx(Of ClassB)(target)
+    End Function
+
+End Class
